0284ebe4e1aa291884d33bf645ccc32511bbee44
armmmartirosyann <armenmartirosyan020@gmail.com>
2024-02-02 14:12:09 +0400

    fixed the problems, added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,29 @@ const RULES = [
     {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: 'babel-loader'
     }
+];
+
+const PLUGINS = [
+    new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        favicon: "./src/favicons/favicon-32x32.png"
+    })
 ];
 
 module.exports = {
     entry: './src/index.tsx',
-    mode: "development",
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ],
     resolve: {
-        extensions: [".ts", ".tsx"],
+        extensions: [".tsx", ".ts", ".js"],
     },
     module: {
         rules: RULES,
     },
+    plugins: PLUGINS,
     devServer: {
-        port: 3000
+        port: 3000,
+        open: true,
+        hot: true
     }
 };
